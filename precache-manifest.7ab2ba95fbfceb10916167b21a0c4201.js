ffadc5b309485cc8aa5d4ef93247d80fade77a1b
zaryab <zaryabbkh@gmail.com>
2023-04-10 11:14:52 +0500

    Updates

---
+++ precache-manifest.7ab2ba95fbfceb10916167b21a0c4201.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e466ebe66db647ef4a528c2129248a38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b210e71a151704c8b4",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "976a4a8d5d90a268beed",
+    "url": "/static/css/main.c9d9d0c1.chunk.css"
+  },
+  {
+    "revision": "84b210e71a151704c8b4",
+    "url": "/static/js/2.cbadea8f.chunk.js"
+  },
+  {
+    "revision": "d183cebce5a122eec812615f5aa046c4",
+    "url": "/static/js/2.cbadea8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976a4a8d5d90a268beed",
+    "url": "/static/js/main.5ac2a7ef.chunk.js"
+  },
+  {
+    "revision": "b68a27077fcfee8256bf",
+    "url": "/static/js/runtime-main.d265118d.js"
+  },
+  {
+    "revision": "82c039d373dad325c2328f2de96f134c",
+    "url": "/static/media/GSU-Bold.82c039d3.ttf"
+  },
+  {
+    "revision": "766ff5661355f76ec536293fc7f43eb7",
+    "url": "/static/media/GSU-Regular.766ff566.ttf"
+  },
+  {
+    "revision": "ae34d97567458beacbefb875915f8750",
+    "url": "/static/media/dai-pixel.ae34d975.png"
+  }
+]);
